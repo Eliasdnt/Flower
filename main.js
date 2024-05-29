97b14f4c5b6435debb25c31b5c435dfc727c43ff
antonio elias <antonio.elias71@aluno.ifce.edu.br>
2024-05-29 11:59:38 -0300

    add permissão da música

--- main.js
+++ main.js
@@ -4,3 +4,13 @@ onload = () => {
       clearTimeout(c);
     }, 1000);
   };
+
+
+  navigator.mediaDevices.getUserMedia({ audio: true })
+    .then(function(stream) {
+        console.log('Permissão concedida para acessar o áudio.');
+        // Use o stream de áudio conforme necessário
+    })
+    .catch(function(err) {
+        console.error('Erro ao obter permissão para acessar o áudio: ', err);
+    });
