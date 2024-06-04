9e4ea77fcaad49e0616b565336e0b05bd78b222e
antonio elias <antonio.elias71@aluno.ifce.edu.br>
2024-06-04 08:59:13 -0300

    remove microphone permission

--- main.js
+++ main.js
@@ -5,12 +5,3 @@ onload = () => {
     }, 1000);
   };
 
-
-  navigator.mediaDevices.getUserMedia({ audio: true })
-    .then(function(stream) {
-        console.log('Permissão concedida para acessar o áudio.');
-        // Use o stream de áudio conforme necessário
-    })
-    .catch(function(err) {
-        console.error('Erro ao obter permissão para acessar o áudio: ', err);
-    });
